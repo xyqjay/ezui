1723490bac147482344b01395456ecfbedf03b8f
xiao.chen <xiao.chen@easygroup.ca>
2021-03-09 16:15:37 +0800

    [new] button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,333 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import Button from './components/button/button';
+// import logo from '../src/asset/111.jpeg';
+import Button from './components/button/button';
+import Input from './components/input/input';
+// import TodoList from './components/todoList/todoList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <Button onclick={()=>{console.log(1)}}></Button> */}
-      </header>
+    <div className="App" >
+      <div style={{ display: 'none', flexDirection: "column" }}>
+        <div>主要按钮</div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {/* <TodoList></TodoList> */}
+          <div className="big">
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big'
+              shape='circle'
+              type='primary'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big' shape='round'
+              type='primary'
+            >Primary</Button>
+            <Button onClick={() => { console.log(1234565) }}
+              size='big'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            ></Button>
+          </div>
+          <div className="middle" style={{ margin: '30px' }}>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle'
+              shape='circle'
+              type='primary'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle' shape='round'
+              type='primary'
+            >Primary</Button>
+            <Button onClick={() => { console.log(1234565) }}
+              size='middle'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+              type='primary'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            ></Button>
+          </div>
+
+          <div className="small" style={{ margin: '30px' }}>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small'
+              type='primary'
+              shape='circle'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small' shape='round'
+              type='primary'
+            >Primary</Button>
+            <Button onClick={() => { console.log(1234565) }}
+              size='small'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+              type='primary'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small'
+              type='primary'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            ></Button>
+          </div>
+
+        </div>
+        <div>次要按钮</div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div className="big">
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big'
+              shape='circle'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big' shape='round'
+            >Primary</Button>
+            <Button onClick={() => { console.log(1234565) }}
+              size='big'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='big'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            ></Button>
+          </div>
+          <div className="middle" style={{ margin: '30px' }}>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle'
+              shape='circle'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle' shape='round'
+            >Primary</Button>
+            <Button onClick={() => { console.log(1234565) }}
+              size='middle'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='middle'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            ></Button>
+          </div>
+
+          <div className="small" style={{ margin: '30px' }}>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small'
+              shape='circle'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small' shape='round'
+            >Primary</Button>
+            <Button onClick={() => { console.log(1234565) }}
+              size='small'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              size='small'
+              icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />}
+              shape='round'
+            ></Button>
+          </div>
+        </div>
+        <div>文字按钮</div>
+
+        <div style={{ margin: '30px' }}>
+
+          <Button
+            onClick={() => { console.log(1234565) }}
+            type='text'
+          >Primary</Button>
+
+          <Button
+            onClick={() => { console.log(1234565) }}
+            icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            type='text'
+          >Primary</Button>
+
+          <Button
+            onClick={() => { console.log(1234565) }}
+            type='link'
+          >Primary</Button>
+
+          <Button
+            onClick={() => { console.log(1234565) }}
+            icon={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} shape='circle'
+            type='link'
+            href='https://lanhuapp.com/web/#/item?fid=all&commonly=all'
+            target='_blank'
+          >Primary</Button>
+        </div>
+        <div>默认   悬浮   点按  禁用</div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ margin: '30px' }}>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='primary'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='primary'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='primary'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='primary'
+              disabled={true}
+            >Primary</Button>
+
+          </div>
+          <div style={{ margin: '30px' }}>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              disabled={true}
+            >Primary</Button>
+
+          </div>
+          <div style={{ margin: '30px' }}>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='text'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='text'
+            >Primary</Button>
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='text'
+            >Primary</Button>
+
+            <Button
+              onClick={() => { console.log(1234565) }}
+              type='text'
+              disabled={true}
+            >Primary1111</Button>
+
+          </div>
+
+        </div>
+      </div>
+      <div style={{ marginBottom: '20px' }}>
+        <Input
+          size='big'
+          prefix={<img src='https://cdn.lumiclass.com/cms/qm/2021/01/25/48a45eaf-9ae7-42b8-8d7b-96e83ea4030a.png' alt="" />} 
+        ></Input>
+        <Input
+          size='big'
+
+        ></Input>
+      </div>
+      <div style={{ marginBottom: '20px' }}>
+        <Input
+          size='middle'
+        ></Input>
+        <Input
+          size='middle'
+
+        ></Input>
+      </div>
+      <div style={{ marginBottom: '20px' }}>
+        <Input
+          size='small'
+        ></Input>
+        <Input
+          size='small'
+        ></Input>
+      </div>
+
     </div>
   );
 }
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,8 +1,90 @@
-function add<T>(arg1: T, arg2: T): T {
-    return arg1 + arg2
+import React, { ReactElement } from 'react';
+import Style from './input.module.scss';
+
+
+interface Props {
+    addonAfter?: ReactElement,//	带标签的 input，设置后置标签	ReactNode	-	
+    addonBefore?: ReactElement,//	带标签的 input，设置前置标签	ReactNode	-	
+    allowClear?: boolean,//	可以点击清除图标删除内容	boolean	-	
+    bordered?: boolean,	//是否有边框	boolean	true	4.5.0
+    defaultValue?: string,//	输入框默认内容	string	-	
+    disabled?: boolean,//是否禁用状态，默认为 false	boolean	false	
+    id?: string//	输入框的 id	string	-	
+    maxLength?: number	//最大长度	number	-	
+    prefix?: ReactElement,//	带有前缀图标的 input	ReactNode	-	
+    size?: string//	控件大小。注：标准表单内的输入框大小限制为 large	large | middle | small	-	
+    suffix?: ReactElement,//	带有后缀图标的 input	ReactNode	-	
+    type?: string//	声明 input 类型，同原生 input 标签的 type 属性，见：MDN(请直接使用 Input.TextArea 代替 type="textarea")	string	text	
+    value?: string//	输入框内容	string	-	
+    onChange?: () => void,//	输入框内容变化时的回调	function(e)	-	
+    onPressEnter?: () => void,//
 }
-// function add(arg1: number, arg2: number): number {
-//     return arg1 + arg2
-// }
-// add(1,2)
-add<number>(1, 2)
+
+function Button({ prefix, size }: Props,) {
+    let inputClassName = `${Style['ez-input']} `;
+    let inputWrapClassName = `${Style['ez-input-affix-wrapper']} `;
+    let inputInnerSpanClassName = `${Style['ez-input-prefix']} `;
+
+
+    switch (size) {
+        case 'big':
+            inputClassName = inputClassName + `${Style['ez-input-bg']} `;
+            inputWrapClassName = inputWrapClassName + `${Style['ez-input-affix-wrapper-bg']} `;
+            break;
+        case 'middle':
+            inputClassName = inputClassName + `${Style['ez-input-md']} `;
+            inputWrapClassName = inputWrapClassName + `${Style['ez-input-affix-wrapper-md']} `;
+            break;
+        case 'small':
+            inputClassName = inputClassName + `${Style['ez-input-sm']} `;
+            inputWrapClassName = inputWrapClassName + `${Style['ez-input-affix-wrapper-sm']} `;
+            break;
+        default:
+            inputClassName = inputClassName + '';
+            break;
+    }
+    // switch (type) {
+    //     case 'primary':
+    //         buttonClasName = buttonClasName + `${Style['ez-btn-primary']} `;
+    //         break;
+    //     case 'text':
+    //         buttonClasName = buttonClasName + `${Style['ez-btn-text']} `;
+    //         break;
+    //     case 'link':
+    //         buttonClasName = buttonClasName + `${Style['ez-btn-link']} `;
+    //         break;
+    //     default:
+    //         buttonClasName = buttonClasName + '';
+    //         break;
+    // }
+    // switch (shape) {
+    //     case 'round':
+    //         buttonClasName = buttonClasName + `${Style['ez-btn-round']} `;
+    //         break;
+    //     case 'circle':
+    //         buttonClasName = buttonClasName + `${Style['ez-btn-circle']} `;
+    //         break;
+    //     default:
+    //         buttonClasName = buttonClasName + '';
+    //         break;
+    // }
+    // buttonClasName = buttonClasName + (children ? '' : icon ? `${Style['ez-btn-icon-only']} ` : '');
+
+
+    // buttonClasName = buttonClasName + (disabled ? `${Style['ez-btn-disabled']} ` : '');
+
+    return (
+        <div style={{ marginRight: '20px', display: "inline-block" }}>
+            {prefix ?
+                <span className={inputWrapClassName}>
+                    <span className={inputInnerSpanClassName}>
+                        {/* <span className={inputIcon}>{prefix}</span> */}
+                    </span>
+                    <input type="text" className={inputClassName} />
+                </span>
+                : <input type="text" className={inputClassName} />}
+
+        </div>
+    );
+}
+export default Button;
